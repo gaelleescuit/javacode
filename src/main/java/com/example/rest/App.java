8de4f5022cf9ddf034db1852be5e13fedad37018
gaelleescuit <36758951+gaelleescuit@users.noreply.github.com>
2018-05-18 15:18:24 +0200

    Destroy the Reactor Core mission

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -100,7 +100,7 @@ public class App {
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery(query);
             if (rs.next()) System.out.println(rs.getInt(dimension));
-            return rs.getInt(dimension);
+            runDatabaseQuery= rs.getInt(dimension);
         } catch (Exception e) {
             System.out.println(e);
         }
